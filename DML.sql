aa5cc643ea8f3f77f439b294157e70e581d91c34
Marcos Camargo <marcoscrcamargo@gmail.com>
2017-06-27 10:26:04 -0300

    modificações finalizadas, falta testar pois SQLDeveloper não funciona

--- DML.sql
+++ DML.sql
@@ -12,9 +12,9 @@ INSERT INTO Nacao(Nome, Continente, Modalidade) VALUES ('Brasil', 'America', 1);
 INSERT INTO Nacao(Nome, Continente, Modalidade) VALUES ('Japão', 'Asia', 2);
 INSERT INTO Nacao(Nome, Continente, Modalidade) VALUES ('Indonesia', 'Asia', 3);
 
-INSERT INTO Atleta VALUES (1, 'Marcos', 'M', 1.68, 70.0, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Brasil');
-INSERT INTO Atleta VALUES (2, 'Victor', 'M', 1.68, 70.0, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Indonesia');
-INSERT INTO Atleta VALUES (3, 'Gabriel', 'M', 1.68, 70.0, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Japão');
+INSERT INTO Atleta VALUES (1, 'Marcos Amargo', 'M', 1.68, 70.0, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Brasil');
+INSERT INTO Atleta VALUES (2, 'Victor Forberu', 'M', 1.68, 70.0, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Indonesia');
+INSERT INTO Atleta VALUES (3, 'Gabriel Siemmel Nacismento', 'M', 1.68, 70.0, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Japão');
 
 INSERT INTO Preparador VALUES (1, 'Rodrigo Mello', 'M', 'mello@olimpiadas.com', '12345678', 'São Carlos', 'São Paulo', 'Brasil', 1, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
 INSERT INTO Preparador VALUES (2, 'Moacir Ponti', 'M', 'ponti@olimpiadas.com', '12345678', 'São Carlos', 'São Paulo', 'Brasil', 1, TO_DATE('1997/02/07 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));
@@ -76,3 +76,62 @@ INSERT INTO TipoOcorrenciaOlimpica VALUES (3, 3, TO_DATE('2017/06/06 8:00:00', '
 INSERT INTO ExameDeDoping VALUES (2, 2, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'O Exame resultou em positivo.');
 
 INSERT INTO TesteDeDoping VALUES (2, 2, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 1, 2);
+
+INSERT INTO MetodoDeTratamento VALUES ('J09', 3, 3, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 1, 'Tomar remedios', 'Efetivo');
+INSERT INTO MetodoDeTratamento VALUES ('J09', 1, 1, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 3, 'Repousar', 'Efetividade média');
+INSERT INTO MetodoDeTratamento VALUES ('J30', 2, 2, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 2, 'Remédio Injetavel', 'Muito efetivo');
+
+INSERT INTO Lesao VALUES (1, 1, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'O atleta quebrou a perna');
+INSERT INTO Lesao VALUES (3, 3, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'O atleta torceu o pé');
+
+INSERT INTO ModalidadeAtleta VALUES (1, 1);
+INSERT INTO ModalidadeAtleta VALUES (2, 2);
+INSERT INTO ModalidadeAtleta VALUES (3, 3);
+
+INSERT INTO PreparadorAtleta VALUES (1, 1);
+INSERT INTO PreparadorAtleta VALUES (2, 2);
+INSERT INTO PreparadorAtleta VALUES (3, 3);
+
+INSERT INTO RotinaTreino VALUES (1, 1);
+INSERT INTO RotinaTreino VALUES (2, 2);
+INSERT INTO RotinaTreino VALUES (3, 3);
+
+INSERT INTO TreinoPreparo VALUES (1, 1);
+INSERT INTO TreinoPreparo VALUES (2, 2);
+INSERT INTO TreinoPreparo VALUES (3, 3);
+
+INSERT INTO TreinoAtividadeDeRecuperacao VALUES (1, 1);
+INSERT INTO TreinoAtividadeDeRecuperacao VALUES (2, 2);
+INSERT INTO TreinoAtividadeDeRecuperacao VALUES (3, 3);
+
+INSERT INTO RotinaAtleta VALUES (1, 1);
+INSERT INTO RotinaAtleta VALUES (2, 2);
+INSERT INTO RotinaAtleta VALUES (3, 3);
+
+INSERT INTO RotinaAtleta VALUES (1, 1);
+INSERT INTO RotinaAtleta VALUES (2, 2);
+INSERT INTO RotinaAtleta VALUES (3, 3);
+
+INSERT INTO PreparadorRotina VALUES (1, 1);
+INSERT INTO PreparadorRotina VALUES (2, 2);
+INSERT INTO PreparadorRotina VALUES (3, 3);
+
+INSERT INTO TelefonePreparador VALUES (16, 50506060, 1);
+INSERT INTO TelefonePreparador VALUES (16, 20307070, 2);
+INSERT INTO TelefonePreparador VALUES (16, 20304050, 3);
+
+INSERT INTO TelefoneMedico VALUES (13, 50506060, 1);
+INSERT INTO TelefoneMedico VALUES (13, 20307070, 2);
+INSERT INTO TelefoneMedico VALUES (13, 20304050, 3);
+
+INSERT INTO RotinaDiaDaSemana VALUES (1, 'Segunda');
+INSERT INTO RotinaDiaDaSemana VALUES (2, 'Terca');
+INSERT INTO RotinaDiaDaSemana VALUES (3, 'Quarta');
+
+INSERT INTO EnderecoMedico VALUES (1, 'Carlos de Camargo Salles', 306, 'ap 2', 13550650, 'São Carlos', 'São Paulo');
+INSERT INTO EnderecoMedico VALUES (2, 'Carlos de Camargo Salles', 306, 'ap 2', 13550650, 'São Carlos', 'São Paulo');
+INSERT INTO EnderecoMedico VALUES (3, 'Carlos de Camargo Salles', 306, 'ap 2', 13550650, 'São Carlos', 'São Paulo');
+
+INSERT INTO Sintoma VALUES (1, 1, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Nariz escorrendo, falta de ar');
+INSERT INTO Sintoma VALUES (2, 2, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Dor de Cabeça');
+INSERT INTO Sintoma VALUES (3, 3, TO_DATE('2017/06/06 8:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Dor no pé');
