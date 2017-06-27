81622cda65f7d2399378265d11f6e09fc9d9ad65
Marcos Camargo <marcoscrcamargo@gmail.com>
2017-06-27 15:36:21 -0300

    Relatório 4

--- RELATORIOS.sql
+++ RELATORIOS.sql
@@ -7,17 +7,31 @@ SELECT A.NOME, A.PASSAPORTE, A.NACAO, A.NASCIMENTO
                 JOIN MEDICO M ON M.ID=AMT.MEDICO
                 JOIN PREPARADORATLETA PA ON PA.ATLETA=A.PASSAPORTE
                 JOIN PREPARADOR P ON PA.PREPARADOR=P.PASSAPORTE
-                WHERE (ME.NOME='Futebol' AND M.NOME='Rafoso' AND P.NOME='Rodrigo Mello');
+        WHERE (ME.NOME='Futebol' AND M.NOME='Rafoso' AND P.NOME='Rodrigo Mello');
 -- ME = MODALIDADE, M = MEDICO, P = PREPARADOR
 
 -- 2) liste todos os médicos que atenderam pelo menos X atletas  de uma nação N, juntamente ao número total de atletas que atenderam, em que X e  são definidos pelo usuário
 SELECT M.NOME, COUNT(*) AS ATLETAS_ATENDIDOS
     FROM MEDICO M JOIN ATLETAMEDICOATENDE AMA ON M.ID=AMA.MEDICO
                 JOIN ATLETA A ON A.PASSAPORTE=AMA.ATLETA
-                WHERE (A.NACAO='Brasil') -- NAÇÃO N
-                GROUP BY M.NOME
-                HAVING COUNT(*) >= 1; -- ATLETAS X
+        WHERE (A.NACAO='Brasil') -- NAÇÃO N
+        GROUP BY M.NOME
+        HAVING COUNT(*) >= 1; -- ATLETAS X
 
 
--- 4) Identifique os treinadores com o maior número de atletas em situação  irregular  devido  ao  doping,  exibindo  os  resultados  da  razão  entre  o  número  de  atletas irregulares pelo total de atletas treinados pelo mesmo treinador
+-- 4) Identifique os treinadores com o maior número de atletas em situação irregular  devido  ao  doping,
+--    exibindo  os  resultados  da  razão  entre  o  número  de  atletas irregulares pelo total de atletas treinados pelo mesmo treinador
 
+SELECT NAT.NOME, NAT.N_ATLETAS_TREINADOS, COALESCE(NAI.N_ATLETAS_IRREGULARES,0),COALESCE(NAI.N_ATLETAS_IRREGULARES/NAT.N_ATLETAS_TREINADOS, 0) AS RAZAO
+FROM
+    (SELECT P.NOME, COUNT(*) AS N_ATLETAS_TREINADOS
+        FROM PREPARADOR P
+        GROUP BY P.NOME) NAT
+LEFT JOIN        
+    (SELECT P.NOME, COUNT(*) AS N_ATLETAS_IRREGULARES
+        FROM PREPARADOR P JOIN PREPARADORATLETA PA ON P.PASSAPORTE = PA.PREPARADOR
+                          JOIN EXAMEDEDOPING ED ON PA.ATLETA = ED.ATLETA
+        WHERE ED.DESRICAODOSRESULTADOS LIKE '%positivo%'
+        GROUP BY P.NOME) NAI
+ON NAT.NOME = NAI.NOME
+ORDER BY NAI.N_ATLETAS_IRREGULARES ASC;
